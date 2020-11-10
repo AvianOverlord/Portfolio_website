428775211411513164ee2ce76c31b61359c31fce
William Rave <wrave@gwu.edu>
2020-11-10 17:21:34 -0500

    Updated format for entries, added borders.

--- src/components/entry.js
+++ src/components/entry.js
@@ -11,9 +11,9 @@ class Entry extends React.Component
     {
         return(
             <div className="card entry">
-                {this.props.noBanner !== true && this.props.art !==null && <img src={this.props.art} className="card-img-top" alt="Card Banner"/>}
+                {this.props.useBanner === true && this.props.art !==null && <img src={this.props.art} className="card-img-top entry-image" alt="Card Banner"/>}
                 <div className="card-body">
-                    {this.props.noBanner === true && <img src={this.props.art} alt="Card Image"/>}
+                    {this.props.useBanner !== true && this.props.art !==null && <img src={this.props.art} className="entry-image" alt="Card Image"/>}
                     {this.props.link !== null && <h5 className="card-title"><a href={this.props.link}>{this.props.title}</a></h5>}
                     {this.props.link === null && <h5 className="card-title">{this.props.title}</h5>}
                     <p className="card-text">{this.props.text0}</p>

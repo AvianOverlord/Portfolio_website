3f8191c3088ef437022b7fad30fe0c166880212d
William Rave <wrave@gwu.edu>
2020-11-10 17:21:49 -0500

    Added TNH website entry

--- src/pages/coding.js
+++ src/pages/coding.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Entry from "../components/entry";
+import TNHscreen from "../assets/tnhscreen.png"
 
 function Coding()
 {
@@ -10,6 +11,12 @@ function Coding()
                 art = {null}
                 title = "Portfolio Website"
                 text0 = "This portfolio website was built from the ground up in React."
+            />
+            <Entry
+                art = {TNHscreen}
+                title= "New Humans Website"
+                link = "https://newhumanssite.com/"
+                text0 = "I maintain the New Humans website on wordpress, including implementing non-standard features. I'm particularly proud of the footnotes."
             />
         </div>
     )
--- src/pages/writing.js
+++ src/pages/writing.js
@@ -10,6 +10,7 @@ function Writing()
             <h2>Writing and Editing</h2>
             <Entry 
                 art={TNH}
+                useBanner = {true}
                 title="The New Humans"
                 text0 ="The New Humans is a superhero-adjacient web novel which I edit for my friend Henry Walker. It's set in 1960's Australia, and centers on a group of superpowered children and the world of heroes, monsters, and cold wars they're forced into." 
                 text1 ="The New Humans has been posted for several years and has over 200 subscribed readers."
@@ -17,7 +18,6 @@ function Writing()
             />
             <Entry
                 art = {WB}
-                noBanner = {true}
                 title="Wise Blood"
                 text0 = "Wise Blood is a web novel I edit, also written by Henry Walker. A boy becomes a vampire after a drug deal goes horribly wrong, and must navigate the hidden world he is now a member off."
                 link = "https://www.royalroad.com/fiction/36197/wise-blood"
